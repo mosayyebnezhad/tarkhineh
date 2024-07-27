c1286811b94065ef682afb7c194e1b523aecddde
mosayyebnezhad <mohammadmosayyebnezhad@gmail.com>
2024-07-27 09:58:34 +0330

    edit back

--- Back/src/Pages/Contacts/ContactUs.tsx
+++
@@ -1,9 +0,0 @@
-import Buttons from "../../Components/Buttons/buttons";
-
-const ContactUs = () => {
-    return (
-        <Buttons btnStyle="filled" color="white" size="medium" >here is Contat us</Buttons>
-    )
-}
-
-export default ContactUs;
--- Back/src/Pages/Notfound/notfound.tsx
+++
@@ -1,8 +0,0 @@
-const Notfound = () => {
-    return (
-        <h1>404 Not Found</h1>
-    )
-}
-
-
-export default Notfound;
