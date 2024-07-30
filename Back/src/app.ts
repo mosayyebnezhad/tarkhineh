d41bc615bbbfe1d17dfaf9e2e6d7dd1b2c50881b
mosayyebnezhad <mohammadmosayyebnezhad@gmail.com>
2024-07-30 05:41:29 +0330

    update

--- Back/src/app.ts
+++ Back/src/app.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import Data from "./db.json"
+import env from 'dotenv';
 const app = express();
-const PORT = process.env.PORT || 5000;
+env.config();
+const PORT = process.env.PORT;
 app.get("/" , (req,res)=>{
   
-  return res.json(Data.users);
+  // return res.json(Data.users);
+ return res.json(process.env.PORT);
 })
 
-app.listen(PORT, () =>
-  console.log(`server running in http://localhost:${PORT}`)
+app.listen(3000, () =>
+  console.log(`server running in http://localhost:${3000}`)
 );
